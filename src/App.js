a664acfe14480fcb78bb7a402aafb7c3d6175014
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-09-30 12:46:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { config, useSpring, animated } from 'react-spring'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [flip, set] = useState(false)
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    config: config.molasses,
+    onRest: () => set(!flip),
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="animation">
+      <animated.h1 style={props}>hello</animated.h1>
+    </div>)
 }
 
 export default App;
